1e51295d8bf2de50cd4406ee605177b48dc4a9bf
Akshay Gandhi <akshay.gandhi@tekditechnologies.com>
2025-05-22 13:07:27 +0530

    Revert "fix: get auth-token from session storage"
    This reverts commit de6a4f597332741f109adb7329cfffed283dc518.

--- frontend/web/micro-ui-internals/packages/modules/PublicServices/src/pages/employee/DigitDemo/DigitDemoSearch.js
+++ frontend/web/micro-ui-internals/packages/modules/PublicServices/src/pages/employee/DigitDemo/DigitDemoSearch.js
@@ -24,7 +24,7 @@ const DigitDemoSearch = () => {
           params: { tenantId : tenantId },
           headers: {
             "X-Tenant-Id": tenantId,
-            "auth-token" : Digit.SessionStorage.get("User")?.access_token,
+            "auth-token" : window?.localStorage?.getItem("Employee.token"),
           },
         });
         setData(response.data);
--- frontend/web/micro-ui-internals/packages/modules/PublicServices/src/pages/employee/DigitDemo/InboxService.js
+++ frontend/web/micro-ui-internals/packages/modules/PublicServices/src/pages/employee/DigitDemo/InboxService.js
@@ -23,7 +23,7 @@ const InboxService = () => {
           params: { tenantId: tenantId },
           headers: {
             "X-Tenant-Id": tenantId,
-            "auth-token" : Digit.SessionStorage.get("User")?.access_token,
+            "auth-token" : window?.localStorage?.getItem("Employee.token"),
           },
         });
 
