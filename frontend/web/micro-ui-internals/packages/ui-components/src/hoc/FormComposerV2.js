7c0103b023f691e6b22bfb13fff3074aecc0b8bc
Akshay Gandhi <akshay.gandhi@tekditechnologies.com>
2025-05-23 16:32:52 +0530

    fix: remove comment part

--- frontend/web/micro-ui-internals/packages/ui-components/src/hoc/FormComposerV2.js
+++ frontend/web/micro-ui-internals/packages/ui-components/src/hoc/FormComposerV2.js
@@ -459,7 +459,7 @@ export const FormComposer = (props) => {
           <SubmitBar label={t(props.label)} className="digit-formcomposer-submitbar" submit="submit" disabled={isDisabled} />
           {props.onSkip && props.showSkip && <ActionLinks style={props?.skipStyle} label={t(`CS_SKIP_CONTINUE`)} onClick={props.onSkip} />}
         </Footer>
-      )} */}
+      )}
       {showErrorToast && <Toast type={"error"} label={t("ES_COMMON_PLEASE_ENTER_ALL_MANDATORY_FIELDS")} isDleteBtn={true} onClose={closeToast} />}
       {customToast && <Toast type={customToast?.type} label={t(customToast?.label)} isDleteBtn={true} onClose={closeToast} />}
     </form>
